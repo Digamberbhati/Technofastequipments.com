8f38f494187ca0b502063faf4ee1748c5122441f
Gaurav-vashisth <sharmagaurav1764@gmail.com>
2024-10-07 22:15:54 +0530

    ok

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,7 +8,7 @@ export function NavbarMenu() {
   return (
     <div className="relative w-full   flex items-center justify-center">
       <Navbar className="bg-zinc-900 " />
-    
+
     </div>
   );
 }
@@ -20,42 +20,44 @@ function Navbar({ className }: { className?: string }) {
       className={cn("fixed top-0    w-full z-50", className)}
     >
       <Menu setActive={setActive} >
-<div className="flex w-full items-center  justify-between py-4  ">
-<div className="text-2xl font-bold">
-LOGO
-</div>
-
-<div className="flex items-center justify-center gap-10 " >
-
-
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/service">Service</Link>
-        <Link href="/careers">Careers</Link>
-        <Link href="/contact">Contact</Link>
-
-        <MenuItem setActive={setActive} active={active} item="Comapny">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/client">Client</HoveredLink>
-            <HoveredLink href="/support">Support</HoveredLink>
+        <div className="flex w-full items-center  justify-between py-4  ">
+          <div className="text-2xl font-bold">
+            LOGO
           </div>
-        </MenuItem>
-</div>
-</div>
-       
+
+          <div className="flex items-center justify-center gap-10 " >
+
+
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/service">Service</Link>
+            <Link href="/careers">Careers</Link>
+            <Link href="/contact">Contact</Link>
+
+            <MenuItem setActive={setActive} active={active} item="Comapny">
+              <div className="flex flex-col space-y-4 text-sm">
+                <HoveredLink href="/client">Client</HoveredLink>
+                <HoveredLink href="/support">Support</HoveredLink>
+              </div>
+            </MenuItem>
+          </div>
+        </div>
+
       </Menu>
+<body>
   
-    
-          {/* Hero Section */}
-        
-  
-    
-  
-    
-    
-    
-          </div>
-    
+      </body>
+
+      {/* Hero Section */}
+
+
+
+
+
+
+
+    </div>
+
   );
 }
 
